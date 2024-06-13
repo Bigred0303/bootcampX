c7bcefc1d9c9ca2b7aeaaf37e6fac99845f39d51
Grant <grantimaxi03@gmail.com>
2024-06-13 07:58:36 -0400

    Change queries to parameterized version

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -1,22 +1,26 @@
-const { Pool } = require("pg");
+const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: "development",
-  password: "development",
-  host: "localhost",
-  database: "bootcampx",
+  user: 'development',
+  password: 'development',
+  host: 'localhost',
+  database: 'bootcampx',
 });
 
-pool
-  .query(
-    `
-SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-FROM students
-JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-`
-  )
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+
+const queryString = `
+  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+`;
+
+const values = [`%${cohortName}%`, limit];
+
+pool.query(queryString, values)
   .then((res) => {
     res.rows.forEach((user) => {
       console.log(
@@ -24,4 +28,4 @@ LIMIT ${process.argv[3] || 5};
       );
     });
   })
-  .catch((err) => console.error("query error", err.stack));
+  .catch((err) => console.error('query error', err.stack));
